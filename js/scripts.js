a7b441138085c5357fd181a13f61d473377c0808
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-21 10:52:54 -0700

    add some functionality to the constructor as well as start formatting styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,31 @@
 var Order = function(){
     pizzas = [],
-    sauces = [],
-    sides = [],
+    // sauces = [],
+    // sides = [],
     idPizza = 0,
-    idSauces = 0,
-    idSides = 0
+    // idSauces = 0,
+    // idSides = 0
 }
 
-var Toppings = function()
-
-var Pizza = function (size, cheese, toppings){
+var Pizza = function(forName, forAddress, size, cheese, pepperoni, sausage, canadianBacon, bacon, chicken, pineapple, mushrooms, onions, peppers, olives, jalapenos){
+    this.forName = forName;
+    this.forAddress = forAddress;
     this.size = size;
     this.cheese = cheese;
-    this.toppings =  function(){
-        this.pepperoni = pepperoni,
-        this.sausage = sausage,
-        this.canadianBacon = canadianBacon,
-        this.bacon = bacon,
-        this.chicken = chicken,
-        this.pineapple = pineapple,
-        this.mushrooms = mushrooms,
-        this.onions = onions,
-        this.peppers = peppers,
-        this.olives = olives,
-        this.jalapenos = jalapenos
-    };
-    
-}
+    this.pepperoni = pepperoni;
+    this.sausage = sausage;
+    this.canadianBacon = canadianBacon;
+    this.bacon = bacon;
+    this.chicken = chicken;
+    this.pineapple = pineapple;
+    this.mushrooms = mushrooms;
+    this.onions = onions;
+    this.peppers = peppers;
+    this.olives = olives;
+    this.jalapenos = jalapenos;
+    } 
 
-var testPizza = {
-    size: "Large",
-    cheese: 1,
-    toppings = {
-        pepperoni: 1,
-        sausage: 0,
-        canadianBacon: 0,
-        bacon: 0,
-        chicken: 0,
-        pineapple: 0,
-        mushrooms: 0,
-        onions: 0,
-        peppers: 0,
-        olives: 0,
-        jalapenos: 0,
-    }
-}
+
+var largePepperoni = new Pizza ("Somename", "someAddress", 3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+var userPizza = new Pizza ()
