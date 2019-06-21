09382aeba76912d645f9957a7b58674a6517d859
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-21 16:13:38 -0700

    Fix functionality of receipt print out to correctly display itemized receipt.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic
 var Pizza = function(forName, forAddress, size, cheese, pepperoni, sausage, canadianBacon, bacon, chicken, pineapple, mushrooms, onions, peppers, olives, jalapenos){
     this.forName = forName;
     this.forAddress = forAddress;
@@ -16,9 +17,9 @@ var Pizza = function(forName, forAddress, size, cheese, pepperoni, sausage, cana
     this.jalapenos = jalapenos;
     } 
 
-Pizza.prototype.pricing = function(){
-    var totalPrice = 0;
-
+    Pizza.prototype.pricing = function(){
+        
+        var totalPrice = 0;
     // size
 
     if (this.size === 1){
@@ -145,22 +146,159 @@ Pizza.prototype.pricing = function(){
 }
 
 
-var largePepperoni = new Pizza ("", "", 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
-var largeCheese = new Pizza ("", "", 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
-var largeMeatLovers = new Pizza ("", "", 3, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1)
-var largeHawaiian = new Pizza ("", "" , 3, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2)
+Pizza.prototype.itemize = function(){
+    var itemizedArray = [];   
+    
+    if (this.size === 1){
+        itemizedArray.push("Size: Small");
+    } else if (this.size === 2){
+        itemizedArray.push("Size: Medium");
+    }  else if (this.size === 3){
+        itemizedArray.push("Size: Large");
+    }  else if (this.size === 4){
+        itemizedArray.push("Size: Xtra-Large");
+    }
+// cheese
+    if (this.cheese === 1){
+        itemizedArray.push("Cheese: None");
+    } else if (this.cheese === 2){
+        itemizedArray.push("Cheese: Regular");
+    }  else if (this.cheese === 3){
+        itemizedArray.push("Cheese: Extra");
+    } 
+// pepperoni
+    if (this.pepperoni === 1){
+        itemizedArray.push("");
+    } else if (this.pepperoni === 2){
+        itemizedArray.push("Pepperoni: Regular");
+    }  else if (this.pepperoni === 3){
+        itemizedArray.push("Pepperoni: Xtra");
+    } 
+// sausage
+    if (this.sausage === 1){
+        itemizedArray.push("");
+    } else if (this.sausage === 2){
+        itemizedArray.push("Sausage: Regular");
+    }  else if (this.sausage === 3){
+        itemizedArray.push("Sausage: Xtra");
+    } 
+// canadian bacon
 
+    if (this.canadianBacon === 1){
+        itemizedArray.push("");
+    } else if (this.canadianBacon === 2){
+        itemizedArray.push("Canadian Bacon: Regular");
+    }  else if (this.canadianBacon === 3){
+        itemizedArray.push("Canadian Bacon: Xtra");
+    } 
+
+    // bacon
+
+    if (this.bacon === 1){
+        itemizedArray.push("");
+    } else if (this.bacon === 2){
+        itemizedArray.push("Bacon: Regular");
+    }  else if (this.bacon === 3){
+        itemizedArray.push("Bacon: Xtra");
+    } 
+    // chicken
+
+    if (this.chicken === 1){
+        itemizedArray.push("");
+    } else if (this.chicken === 2){
+        itemizedArray.push("Chicken: Regular");
+    }  else if (this.chicken === 3){
+        itemizedArray.push("Chicken: Xtra");
+    } 
+
+    // pineapple
+
+    if (this.pineapple === 1){
+        itemizedArray.push("");
+    } else if (this.pineapple === 2){
+        itemizedArray.push("Pineapple: Regular");
+    }  else if (this.pineapple === 3){
+        itemizedArray.push("Pineapple: Xtra");
+    } 
+
+    // mushrooms
+
+    if (this.mushrooms === 1){
+        itemizedArray.push("");
+    } else if (this.mushrooms === 2){
+        itemizedArray.push("Mushrooms: Regular");
+    }  else if (this.mushrooms === 3){
+        itemizedArray.push("Mushrooms: Xtra");
+    } 
+
+    // onions
+
+    if (this.onions === 1){
+        itemizedArray.push("");
+    } else if (this.onions === 2){
+        itemizedArray.push("Onios: Regular");
+    }  else if (this.onions === 3){
+        itemizedArray.push("Onios: Xtra");
+    } 
+    // peppers
+
+    if (this.peppers === 1){
+        itemizedArray.push("");
+    } else if (this.peppers === 2){
+        itemizedArray.push("Peppers: Regular");
+    }  else if (this.peppers === 3){
+        itemizedArray.push("Peppers: Xtra");
+    } 
+
+    // olives
+
+    if (this.olives === 1){
+        itemizedArray.push("");
+    } else if (this.olives === 2){
+        itemizedArray.push("Olives: Regular");
+    }  else if (this.olives === 3){
+        itemizedArray.push("Olives: Xtra");
+    } 
+    // jalapenos
+
+    if (this.jalapenos === 1){
+        itemizedArray.push("");
+    } else if (this.jalapenos === 2){
+        itemizedArray.push("Jalapenos: Regular");
+    }  else if (this.jalapenos === 3){
+        itemizedArray.push("Jalapenos: Xtra");
+    } 
+    return itemizedArray;
+}
+
+
+
+// Front End
 $(document).ready(function(){
     $("#pizzaForm").submit(function(event){
+        
         var userPizza = new Pizza ($("#nameField").val(), $("#addressField").val(), parseInt($("input:radio[name=size]:checked").val()), parseInt($("input:radio[name=cheese]:checked").val()), parseInt($("input:radio[name=pepperoni]:checked").val()), parseInt($("input:radio[name=sausage]:checked").val()), parseInt($("input:radio[name=canadianBacon]:checked").val()), parseInt($("input:radio[name=bacon]:checked").val()), parseInt($("input:radio[name=chicken]:checked").val()), parseInt($("input:radio[name=pineapple]:checked").val()), parseInt($("input:radio[name=mushrooms]:checked").val()), parseInt($("input:radio[name=onions]:checked").val()), parseInt($("input:radio[name=peppers]:checked").val()), parseInt($("input:radio[name=olives]:checked").val()), parseInt($("input:radio[name=jalapenos]:checked").val()));
-        console.log(userPizza.pricing());
+        debugger;
+        var userPizzaPrice = userPizza.pricing();
+        var userPizzaArray = userPizza.itemize();
+
+        $("#receiptPopulate").html("<h4>Hey there, " + userPizza.forName + "!</h4><br><h5>Your order is on the way to " + userPizza.forAddress + ".</h5><br><br><h3>Here is your receipt:</h3><br><h5>Your total today is $" + userPizzaPrice + ".00.</h5><br><h3>Items:</h3><br><h5> " + userPizzaArray[0] + "</h5><br><h5>" + userPizzaArray[1] + "</h5><br><h5>" + userPizzaArray[2] + "</h5><br><h5>" + userPizzaArray[3] + "</h5><br><h5>" + userPizzaArray[4] + "</h5><br><h5>" + userPizzaArray[5] + "</h5><br><h5>" + userPizzaArray[6] + "</h5><br><h5>" + userPizzaArray[7] + "</h5><br><h5>" + userPizzaArray[8] + "</h5><br><h5>" + userPizzaArray[9] + "</h5><br><h5>" + userPizzaArray[10] + "</h5><br><h5>" + userPizzaArray[11] + "</h5><br><h5>" + userPizzaArray[12] + "</h5>")
+        
+        $("#receiptDescriptor").slideUp(100);
+        $("#receiptCard").slideDown(500);
         event.preventDefault();
-
-        console.log(userPizza.forName, userPizza.forAddress, userPizza.size);
-        console.log($("#nameField").val());
+        
     });
-
-// Button logic for non-submit buttons
-
+    
+    // Button logic for non-submit buttons
+    $("#continueButton").click(function(){
+        $("#chooseOptionCard").slideDown(500);
+        $("#continueButton").slideUp(500);
+    });
+    $("#constructButton").click(function(){
+        $("#hiddenPizzaForm").slideDown(500);
+        $("#startPage").slideUp(500);
+    });
+    
 });
 
