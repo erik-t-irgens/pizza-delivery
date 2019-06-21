06296f27ed47a9e5bd7a4b7170ca44662a58b479
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-21 10:06:44 -0700

    add some backend functionality to create a pizza object.

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+var Order = function(){
+    pizzas = [],
+    sauces = [],
+    sides = [],
+    idPizza = 0,
+    idSauces = 0,
+    idSides = 0
+}
+
+var Toppings = function()
+
+var Pizza = function (size, cheese, toppings){
+    this.size = size;
+    this.cheese = cheese;
+    this.toppings =  function(){
+        this.pepperoni = pepperoni,
+        this.sausage = sausage,
+        this.canadianBacon = canadianBacon,
+        this.bacon = bacon,
+        this.chicken = chicken,
+        this.pineapple = pineapple,
+        this.mushrooms = mushrooms,
+        this.onions = onions,
+        this.peppers = peppers,
+        this.olives = olives,
+        this.jalapenos = jalapenos
+    };
+    
+}
+
+var testPizza = {
+    size: "Large",
+    cheese: 1,
+    toppings = {
+        pepperoni: 1,
+        sausage: 0,
+        canadianBacon: 0,
+        bacon: 0,
+        chicken: 0,
+        pineapple: 0,
+        mushrooms: 0,
+        onions: 0,
+        peppers: 0,
+        olives: 0,
+        jalapenos: 0,
+    }
+}
